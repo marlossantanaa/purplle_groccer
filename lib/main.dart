ab45bd7d28341fdc0ce1dbe6b359c48216eda67f
marlossantana <marlossantanaa@gmail.com>
2023-06-06 14:02:03 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:purplle_groccer/src/auth/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sign in',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+       scaffoldBackgroundColor: Colors.grey.shade100,
+       // scaffoldBackgroundColor: Colors.white.withAlpha(1000),
+        splashColor: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+
+      debugShowCheckedModeBanner: false
+      ,
+      home: const SignInScreen(),
+    );
+  }
+}
---
+++ lib/src/config/CustomColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomColors{
+  static Color primary = Colors.purple;
+  static Color secondary = Colors.amber;
+  static Color textColor = Colors.black;
+  static Color textColor2 = Colors.white;
+
+}
